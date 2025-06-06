e0b79ed2330ada4a1d9a1d2a033ce1eaeb3515fb
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-06 16:37:26 +0900

    [feat] 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Home from './pages/home/home.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/home/home.tsx';
+import Menu from './pages/menu/menu.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Home />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
