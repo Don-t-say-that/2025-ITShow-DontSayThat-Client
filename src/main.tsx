0c844b584aa76813a83e557a857b71036852954a
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-07 01:25:27 +0900

    [feat] 사용자 등록 라우터 추가

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home.tsx';
 import Menu from './pages/menu/menu.tsx';
 import JoinGame from './pages/joinGame/joinGame.tsx';
+import RegisterUser from './pages/registerUser/registerUser.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,6 +14,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
         <Route path="/joinGame" element={<JoinGame />} />
+        <Route path= "/registerUser" element={<RegisterUser/> } />
       </Routes>
     </BrowserRouter>
   </StrictMode>
