2f8bd188d80d428bf11fb30cf9a596c5646b14f1
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-07 01:27:05 +0900

    [feat] textInput 컴포넌트 생성

---
+++ src/components/textInput/TextInput.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styles from './textInput.module.css';
+
+interface TextInputProps {
+  placeholder: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  width?: string; 
+  height?: string; 
+}
+
+const TextInput: React.FC<TextInputProps> = ({
+  placeholder,
+  value,
+  onChange,
+  width = '590px',
+  height = '100px', 
+}) => {
+  return (
+    <input
+      className={styles.textInput}
+      type="text"
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      style={{ width, height }}
+    />
+  );
+};
+
+export default TextInput;
