a55fb0317528bb21134bf0a0a7fbe097cd3c3c8b
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-09 16:56:37 +0900

    [feat] 게임 방 생성 및 api 연결

---
+++ src/pages/createRoom/createRoom.tsx
@@ -0,0 +1,75 @@
+import styles from './createRoom.module.css'
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import '../../App.css'
+import TextInput from '../../components/textInput/TextInput'
+import ActionButton from '../../components/ActionButton/ActionButton';
+import Modal from '../../components/Modal/Modal';
+import useUserStore from '../../store/userStore';
+import useRoomStore from '../../store/roomStore';
+import useModalStore from '../../store/ModalStore';
+
+function CreateRoom() {
+  const { roomName, setRoomName } = useRoomStore();
+  const { showModal, setShowModal } = useModalStore();
+
+  const handleroomNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setRoomName(e.target.value);
+  };
+
+  const navigate = useNavigate();
+  
+  const userId = useUserStore((state) => state.id); 
+
+  const handleCreateRoom = async () => {
+    if (!userId) {
+      console.error('유저가 로그인되지 않았습니다.');
+      return;
+    }
+
+    try {
+      const response = await axios.post('http://localhost:3000/teams', {
+        name: roomName,
+        leaderId: userId,
+      });
+
+      const createdTeam = response.data;
+      console.log('생성된 팀:', createdTeam);
+      navigate('/joinGame');
+    } catch (error: any) {
+      if (error.response && error.response.status === 400) {
+        setShowModal(true);
+      } else {
+        console.error('방 생성 중 오류:', error);
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.background}>
+        <p className={styles.title}>방 만들기</p>
+        <div className={styles.inputContainer}>
+          <TextInput
+            placeholder="게임방 이름 입력"
+            value={roomName}
+            onChange={handleroomNameChange}
+            width="41.3vw"
+          />
+        </div>
+        
+        <ActionButton onClick={handleCreateRoom}>생성하기</ActionButton>
+
+        {showModal && (
+          <Modal onClick={() => setShowModal(false)}>
+            게임방 이름이 중복되었습니다. <br/>
+            다시 입력해주세요.
+          </Modal>
+        )}
+
+      </div>
+    </>
+  );
+}
+
+export default CreateRoom;
