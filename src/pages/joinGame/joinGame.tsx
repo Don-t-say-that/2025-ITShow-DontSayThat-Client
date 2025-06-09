8f0041581117d2153d08b4fe0a2746df09b5a203
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-09 16:56:08 +0900

    [feat] 방 목록 불러오기

--- src/pages/joinGame/joinGame.tsx
+++ src/pages/joinGame/joinGame.tsx
@@ -1,28 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './joinGame.module.css';
 import '../../App.css';
 import RoomButton from '../../components/roomButton/RoomButton';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 interface Room {
   id: number;
   name: string;
+  userCount: number;
 }
 
 function JoinGame() {
   const [rooms, setRooms] = useState<Room[]>([]);
-
   const navigate = useNavigate();
-  const handleBackgroundClick = () => {
-    navigate('/registerUser');
-  };
 
-  const createRoom = () => {
-    const newRoom: Room = {
-      id: rooms.length + 1,
-      name: `방 ${rooms.length + 1}`,
+  useEffect(() => {
+    const fetchRooms = async () => {
+      try {
+        const response = await axios.get<Room[]>('http://localhost:3000/teams/waiting');
+        setRooms(response.data);
+      } catch (error) {
+        console.error('방 목록 불러오기 실패:', error);
+      }
     };
-    setRooms([...rooms, newRoom]);
+
+    fetchRooms();
+  }, []);
+
+  const handleCreateUser = () => {
+    navigate('/registerUser');
   };
 
   return (
@@ -32,14 +39,14 @@ function JoinGame() {
         <div className={styles.roomList}>
           {rooms.map((room) => (
             <RoomButton
-                key={room.id}
-                roomName={room.name}
-                currentCount={1}  // 임시로
-                onClick={() => alert(`${room.name} 입장!`)}
+              key={room.id}
+              roomName={room.name}
+              currentCount={room.userCount}
+              onClick={() => alert(`${room.name} 입장!`)}
             />
-            ))}
+          ))}
         </div>
-        <button className={styles.createRoomBtn} onClick={handleBackgroundClick}>
+        <button className={styles.createRoomBtn} onClick={handleCreateUser}>
           +
         </button>
       </div>
