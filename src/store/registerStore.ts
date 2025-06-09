1d0094e61157e053f1bd0287ea1c3c9a17d21351
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-09 16:55:12 +0900

    [refactor] 모달, 사용자 정보 분리해서 관리

--- src/store/registerStore.ts
+++ src/store/registerStore.ts
@@ -3,19 +3,17 @@ import { create } from 'zustand';
 interface RegisterState {
   name: string;
   password: string;
-  showModal: boolean;
   setName: (name: string) => void;
   setPassword: (password: string) => void;
-  setShowModal: (show: boolean) => void;
 }
+
 
 const useRegisterStore = create<RegisterState>((set) => ({
   name: '',
   password: '',
   showModal: false,
   setName: (name) => set({ name }),
-  setPassword: (password) => set({ password }),
-  setShowModal: (show) => set({ showModal: show }),
+  setPassword: (password) => set({ password })
 }));
 
 export default useRegisterStore;
