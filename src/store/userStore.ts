7696b81aa3099a711b89205771fc9ae991ba4889
hyewon4052 <s2311@e-mirim.hs.kr>
2025-06-11 03:39:48 +0900

    [feat] 사용자 준비 상태 zustand로 관리

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -2,14 +2,18 @@ import { create } from 'zustand';
 
 interface UserState {
   id: number | null;
+  isReady: boolean;
   setUserId: (id: number) => void;
+  setIsReady: (userId: number, isReady: boolean) => void;
   resetUser: () => void;
 }
 
 const useUserStore = create<UserState>((set) => ({
   id: null,
+  isReady: false,
   setUserId: (id) => set({ id }),
-  resetUser: () => set({ id: null }),
+  setIsReady: (userId, isReady) => set({ isReady }),
+  resetUser: () => set({ id: null, isReady: false }),
 }));
 
 export default useUserStore;
